445e14a0ec459f663d7f10bba4ac8555e0060e04
George Wu <uiharu@buaa.edu.cn>
2018-01-04 12:47:11 +0100

    fixup

--- build_vanishing.cmd
+++
@@ -1,9 +0,0 @@
-@echo Current directory:
-cd
-cd sp\src\
-@echo Creating projects...
-creategameprojects.bat
-@echo Building...
-msbuild games_vanishing.sln /verbosity:minimal
-cd ..\..\
-@echo Complete.
