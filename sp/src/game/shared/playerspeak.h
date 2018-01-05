7a0d2ad7d86ee8d6b9be75c48faaa1b904cdb603
George Wu <uiharu@buaa.edu.cn>
2018-01-05 18:21:22 +0100

    Player speaking fix and FGD entity (point_playerspeak).

--- sp/src/game/shared/playerspeak.h
+++ sp/src/game/shared/playerspeak.h
@@ -3,36 +3,7 @@
 #define _PLAYER_SPEAK_H_
 
 #include "cbase.h"
-#include <ctype.h>
-#include <KeyValues.h>
-#include "engine/IEngineSound.h"
-#include "SoundEmitterSystem/isoundemittersystembase.h"
-#include "SoundEmitterSystem.h"
-#include "igamesystem.h"
-#include "soundchars.h"
-#include "filesystem.h"
-#include "tier0/vprof.h"
-#include "checksum_crc.h"
-#include "tier0/icommandline.h"
-#include "tier1/strtools.h"
 
-#ifndef CLIENT_DLL
-#include "in_buttons.h"
-#include "util.h"
-#include "envmicrophone.h"
-#include "sceneentity.h"
-#endif
-
-//#ifdef CLIENT_DLL
-#include <vgui_controls/Controls.h>
-#include <vgui/IVgui.h>
-#ifdef CLIENT_DLL
-#include "hud_closecaption.h"
-#endif
-//#endif
-
-#define MAX_SOUND_NAME 80
-
-void UTIL_PlayerSpeak( const char *soundname, float soundtime = 0.0f );
+void UTIL_PlayerSpeak(const char *soundname, float volume, int pitch, float soundtime = 0.0f);
 
 #endif
